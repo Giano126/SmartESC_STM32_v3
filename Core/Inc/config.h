0ca7f001c59425dd6b226634090d1e14eaa33450
Giano126 <112589735+Giano126@users.noreply.github.com>
2023-04-01 15:34:31 +0200

    Speed12

--- Core/Inc/config.h
+++ Core/Inc/config.h
@@ -6,7 +6,7 @@
 // speed limits for invividual modes in kph
 #define SPEEDLIMIT_ECO 40
 #define SPEEDLIMIT_NORMAL 55
-#define SPEEDLIMIT_SPORT 55
+#define SPEEDLIMIT_SPORT 60
 
 // motor current limits for invividual modes in mA
 // note that hacked firmware allows up to 55amps motor phase current
